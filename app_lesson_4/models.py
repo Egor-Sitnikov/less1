6ca4fcbde8dadf4ee999c33437bfa5b02f04a089
Egor-Sitnikov <136489629+Egor-Sitnikov@users.noreply.github.com>
2023-08-08 17:43:29 +0300

    Панель администрирования

--- app_lesson_4/models.py
+++ app_lesson_4/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 
 # Create your models here.
@@ -10,6 +13,23 @@ class Advertisements(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            created_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: orange; font-weight: bold">Сегодня в {}</span>', created_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
 
     class Meta:
         db_table = 'advertisements'
